d85ba20aaa9913446d26ec7261167f7a015b74be
Ronak <soni.ronak88@gmail.com>
2020-12-12 23:52:56 +0530

    Intial Commit

---
+++ src/main/java/com/example/springDemo/SpringDemo/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.example.springDemo.SpringDemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @RequestMapping(value = "/hello", method = RequestMethod.GET)
+    public String getHello() {
+        return "Hello World";
+    }
+}
